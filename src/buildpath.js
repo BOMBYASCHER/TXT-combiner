d48888da9a4b198b962836a37f2a394120877e55
REFAT123321 <119286400+REFAT123321@users.noreply.github.com>
2023-06-13 16:02:58 +0300

    Update project

--- src/buildpath.js
+++ src/buildpath.js
@@ -1,20 +1,9 @@
 import { cwd } from 'node:process';
 import path from 'node:path';
 
-export default (filePath) => {return fs.readdir(filePath)
-      .then((data) => data.map((elem) => {
-        let modeValue = Promise.all(fs.stat(elem)).then((stats) => stats.mode).catch((e) => e)
-        return {
-          filepath: path.join(filePath, elem),
-          mode: modeValue
-        }
-      }))
-      .catch((e) => fs.readFile(filePath)
-          .then((file) => {
-            let modeValue = fs.stat(file).then((stats) => stats.mode).catch((e) => e)
-            return [{filepath: path.join(filePath), mode: modeValue}]}) 
-          .catch((e) => e))}
- {  path.resolve('/etc', 'filePath')
+export default (filePath) => {
+  
+  return path.resolve(cwd(), filePath )
 };
 
 /**
