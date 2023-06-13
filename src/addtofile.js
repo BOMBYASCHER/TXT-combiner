1d1ec5acd36361bd815fb61d002061cf40f9cbed
REFAT123321 <119286400+REFAT123321@users.noreply.github.com>
2023-06-13 14:47:02 +0300

    Update project

--- src/addtofile.js
+++ src/addtofile.js
@@ -1,9 +1,7 @@
 import { appendFile } from 'node:fs';
 
 export default (filePath, data) => {
-const fs = require('fs');
-
-fs.appendFile('message.txt', 'data to append', function (err) {
+('message.txt', 'data to append', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
