30dfe6326bef51b37f8814c63f2108e9f863a367
BOMBYASCHER <bombyascher@gmail.com>
2023-06-14 13:01:39 +0300

    Update tests

--- __tests__/addtofile-test.js
+++ __tests__/addtofile-test.js
@@ -1,5 +1,27 @@
+import { readFile, writeFile, unlink } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'node:path';
 import addtofile from '../src/addtofile.js';
 
-test('Check addtofile', () => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, '..', '__fixtures__', 'testfile-test-addtofile.txt');
 
+afterAll(() => {
+  unlink(filePath, (err) => err);
+});
+
+test('Check addtofile', async () => {
+  writeFile(filePath, '', () => null);
+
+  expect(readFile(filePath, 'utf-8', () => null)).toEqual('');
+
+  addtofile(filePath, 'Hello');
+
+  expect(readFile(filePath, 'utf-8', () => null)).toEqual('Hello');
+
+  addtofile(filePath, 'World!');
+
+  expect(readFile(filePath, 'utf-8', () => null)).toEqual('HelloWorld!');
 });
