05a932c6741be1a17e5cf116565dff301d8da002
BOMBYASCHER <bombyascher@gmail.com>
2023-06-15 14:53:49 +0300

    Update project
    Beta version of CLI

--- bin/txt-combiner.js
+++ bin/txt-combiner.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import combine from '../src/combiner.js';
 
 program
-  .version('1.0.0')
-  .description('The program combines many TXT files into one.')
-  .arguments('<filename> [filestocombine]')
-  .option('-a, --all', 'combine all files in directory')
-  .parse();
+  .version('1.0.1')
+  .description(
+`The program combines many TXT files into one.
+It copies data from files and adds to the file specified by the first parameter.`)
+  .argument('<filename>', 'The file to which the data will be added')
+  .argument('[files]', 'Files from which data will be copied')
+  .action((filename, ...files) => combine(filename, files));
 
-/**
- *
- */
+program.parse(process.argv);
