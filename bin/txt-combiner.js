0bafa50db8502a7517ab640c66b317e381ce8073
BOMBYASCHER <bombyascher@gmail.com>
2023-06-15 14:58:41 +0300

    Fix linter errors

--- bin/txt-combiner.js
+++ bin/txt-combiner.js
@@ -5,8 +5,9 @@ import combine from '../src/combiner.js';
 program
   .version('1.0.1')
   .description(
-`The program combines many TXT files into one.
-It copies data from files and adds to the file specified by the first parameter.`)
+    `The program combines many TXT files into one.
+It copies data from files and adds to the file specified by the first parameter.`,
+  )
   .argument('<filename>', 'The file to which the data will be added')
   .argument('[files]', 'Files from which data will be copied')
   .action((filename, ...files) => combine(filename, files));
