5d76554cde01f02c2a7a6b72aeba6f0f25209ccb
LoseGameng <losegameng@gmail.com>
2023-06-27 12:37:34 +0300

    background upgrade

---
+++ assets/style/canva.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#bgBanner02",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x60a6ed,
+  backgroundColor: 0x1e1e1e,
+  points: 11.00,
+  maxDistance: 27.00,
+  spacing: 18.00
+  })
